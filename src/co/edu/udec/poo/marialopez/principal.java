d634a6ebaf9e8f4c2d196fde830cece8b4c8c7d7
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-04-16 17:37:06 -0500

    creando clase user

---
+++ src/co/edu/udec/poo/marialopez/principal.java
@@ -0,0 +1,7 @@
+package co.edu.udec.poo.marialopez;
+
+public class principal {
+    public static void main (String[] args){
+
+    }
+}
---
+++ src/dominio/User.java
@@ -0,0 +1,16 @@
+package dominio;
+import java.util.Date;
+    public class User {
+        String nombreUser;
+        String lugarNacimiento;
+        //FotoPerfil fotoPerfil;
+        String intereses;
+        String LugarResidencia;
+        Date fechaNacimiento;
+        protected String urlPersonal;
+        protected String email;
+        protected String numeroCelulaR;
+        private String password;
+
+
+    }
