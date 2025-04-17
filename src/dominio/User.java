bedaace8e8d2b325529c13239ae547a81c86d557
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-04-17 02:00:18 -0500

    se creo el constructor y los setters y getters de la clase User

--- src/dominio/User.java
+++ src/dominio/User.java
@@ -1,6 +1,7 @@
 package dominio;
 import java.util.Date;
-    public class User {
+
+public class User {
         String nombreUser;
         String lugarNacimiento;
         //FotoPerfil fotoPerfil;
@@ -9,8 +10,92 @@ import java.util.Date;
         Date fechaNacimiento;
         protected String urlPersonal;
         protected String email;
-        protected String numeroCelulaR;
+        protected String numeroCelular;
         private String password;
 
+        //creacion constructor
+        public  User (String nombreUser, String urlPersonal, String email, String numeroCelular, String intereses){
+            this.nombreUser = nombreUser;
+            this.urlPersonal = urlPersonal;
+            this.email = email;
+            this.numeroCelular = numeroCelular;
+            this.intereses = intereses;
+            this.fechaNacimiento = new Date();
+        }
 
+    //Creando getters y setters
+
+    public String getNombreUser() {
+        return nombreUser;
     }
+
+    public void setNombreUser(String nombreUser) {
+        this.nombreUser = nombreUser;
+    }
+
+    public String getLugarNacimiento() {
+        return lugarNacimiento;
+    }
+
+    public void setLugarNacimiento(String lugarNacimiento) {
+        this.lugarNacimiento = lugarNacimiento;
+    }
+
+    public String getIntereses() {
+        return intereses;
+    }
+
+    public void setIntereses(String intereses) {
+        this.intereses = intereses;
+    }
+
+    public String getLugarResidencia() {
+        return LugarResidencia;
+    }
+
+    public void setLugarResidencia(String lugarResidencia) {
+        LugarResidencia = lugarResidencia;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getUrlPersonal() {
+        return urlPersonal;
+    }
+
+    public void setUrlPersonal(String urlPersonal) {
+        this.urlPersonal = urlPersonal;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumeroCelular() {
+        return numeroCelular;
+    }
+
+    public void setNumeroCelular(String numeroCelular) {
+        this.numeroCelular = numeroCelular;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
